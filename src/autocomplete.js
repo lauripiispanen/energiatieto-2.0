950a671272869c159e0026e3eecc787fa41a354f
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2013-10-30 23:55:10 +0200

    proxy API calls to heroku if local mongodb not available

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,4 +1,22 @@
 var MongoClient = require('mongodb').MongoClient,
+    http = require('http'),
+    queryFromAPI = function(request, response) {
+        var req = http.request({
+            hostname: 'energiatieto-2-0.herokuapp.com',
+            port: 80,
+            path: '/autocomplete?address=' + request.query.address
+        },
+        function(res) {
+            var body = "";
+            res.on('data', function (chunk) {
+                body += chunk;
+            });
+            res.on('end', function() {
+                response.end(body);
+            })
+        });
+        req.end();
+    },
     autocompleteMiddleware = function(collection) {
         return function(req, res) {
             var address = req.query.address;
@@ -21,18 +39,21 @@ var MongoClient = require('mongodb').MongoClient,
         }
     },
     respond = function(req, res) {
-        MongoClient.connect(process.env.MONGOLAB_URI, function(err, db) {
-            if (err) {
-                respond = function(req, res) {
-                    res.end();
+        try {
+            MongoClient.connect(process.env.MONGOLAB_URI, function(err, db) {
+                if (err) {
+                    respond = queryFromAPI;
+                    console.warn("can't connect", err);
+                } else {
+                    console.log("Connected to", process.env.MONGOLAB_URI);
+                    respond = autocompleteMiddleware(db.collection('buildings'));
                 }
-                console.warn("can't connect", err);
-            } else {
-                console.log("Connected to", process.env.MONGOLAB_URI);
-                respond = autocompleteMiddleware(db.collection('buildings'));
-            }
+                respond(req, res);
+            });
+        } catch (e) {
+            respond = queryFromAPI;
             respond(req, res);
-        });
+        }
     }
 
 module.exports = function(req, res) {
--- src/buildingInfo.js
+++ src/buildingInfo.js
@@ -1,25 +1,46 @@
 var MongoClient = require('mongodb').MongoClient,
-    fail = function(req, res) { res.end(); },
+    http = require('http'),
+    queryFromAPI = function(request, response) {
+        var req = http.request({
+            hostname: 'energiatieto-2-0.herokuapp.com',
+            port: 80,
+            path: '/building?address=' + encodeURIComponent(request.query.address)
+        },
+        function(res) {
+            var body = "";
+            res.on('data', function (chunk) {
+                body += chunk;
+            });
+            res.on('end', function() {
+                response.end(body);
+            })
+        });
+        req.end();
+    },
     buildingInfoMiddleware = function(collection) {
         return function(req, res) {
             var address = req.query.address;
-            collection.find({"address": address}).toArray(function(err, docs) {
+            collection.find({"address": address, "floorArea": {$gt: 0}}).toArray(function(err, docs) {
                 res.end(JSON.stringify(docs));
             });
 
         };
     },
     respond = function(req, res) {
-        MongoClient.connect(process.env.MONGOLAB_URI, function(err, db) {
-            if (err) {
-                respond = fail;
-                console.warn("can't connect", err);
-            } else {
-                console.log("Connected to", process.env.MONGOLAB_URI);
-                respond = buildingInfoMiddleware(db.collection('buildings'));
-            }
-            respond(req, res);
-        });
+        try {
+            MongoClient.connect(process.env.MONGOLAB_URI, function(err, db) {
+                if (err) {
+                    respond = queryFromAPI;
+                    console.warn("can't connect", err);
+                } else {
+                    console.log("Connected to", process.env.MONGOLAB_URI);
+                    respond = buildingInfoMiddleware(db.collection('buildings'));
+                }
+                respond(req, res);
+            });
+        } catch (e) {
+            respond = queryFromAPI;
+        }
     }
 
 module.exports = function(req, res) {
