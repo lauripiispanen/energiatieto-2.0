28ce68d65291a93d2def79a9fdee7803ae9ecc7f
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2013-11-01 15:33:01 +0200

    create homebrewn autocomplete

--- static/js/address-form.js
+++ static/js/address-form.js
@@ -1,5 +1,5 @@
 angular
-    .module("energiatieto-addressform", ["ui.select2", "pubsub"])
+    .module("energiatieto-addressform", ["pubsub"])
     .controller("addressFormController", [
         "$scope",
         "buildingSelectionChannel",
@@ -25,91 +25,163 @@ angular
             $scope.hidden = (state != formActivationChannel.messages.deactivate);
         });
 
+        $scope.selectItem = function(item) {
+            // attempt to get buildings for address, move to next phase if successful, otherwise keep autocompleting
+            $.get("/building", { address: item }).done(function(data) {
+                
+                var d = JSON.parse(data),
+                    buildings =  _.map(d, function(it) {
 
-
-        $scope.select2Options = {
-            placeholder: "Search for a movie",
-            minimumInputLength: 3,
-            query: function(query) {
-                $.get("/autocomplete", { address: query.term }).done(function(data) {
-                    query.callback({
-                        results: convertData(data)
-                    });
-                });
-            }
-        }
-
-        $scope.$watch("address", function(newValue) {
-            if (newValue) {
-                $.get("/building", { address: newValue.text }).done(function(data) {
-                    
-                    var d = JSON.parse(data),
-                        buildings =  _.map(d, function(it) {
-
-                            var x = parseFloat(it.pos.x),
-                                y = parseFloat(it.pos.y),
-                                point = new proj4.Point(x, y),
-                                exteriorPolygon = _.map(it.polygon || [], function(polygonPoint) {
-                                    proj4.transform(src, dst, polygonPoint);
-                                    return {
-                                        lon: polygonPoint.x,
-                                        lat: polygonPoint.y
-                                    };
-                                });
-
-                            proj4.transform(src, dst, point);
-
-                            var building = new Building();
-                            building.floorArea = it.floorArea;
-                            building.floorCount = it.floorCount;
-                            building.address = it.address;
-                            building.buildingYear = it.constructionYear;
-                            building.coordinates = {
-                                    lon: point.x,
-                                    lat: point.y
+                        var x = parseFloat(it.pos.x),
+                            y = parseFloat(it.pos.y),
+                            point = new proj4.Point(x, y),
+                            exteriorPolygon = _.map(it.polygon || [], function(polygonPoint) {
+                                proj4.transform(src, dst, polygonPoint);
+                                return {
+                                    lon: polygonPoint.x,
+                                    lat: polygonPoint.y
                                 };
-                            switch(it.fuel) {
-                                case "Kauko- tai aluelämpö":
-                                    building.heatingSystem = "1";
-                                    break;
-                                case "Kevyt polttoöljy":
-                                    building.heatingSystem = "2";
-                                    break;
-                                case "Sähkö":
-                                    building.heatingSystem = "3";
-                                    break;
-                                default:
-                                    building.heatingSystem = "4";
-                                    break;
+                            });
+
+                        proj4.transform(src, dst, point);
+
+                        var building = new Building();
+                        building.floorArea = it.floorArea;
+                        building.floorCount = it.floorCount;
+                        building.address = it.address;
+                        building.buildingYear = it.constructionYear;
+                        building.coordinates = {
+                                lon: point.x,
+                                lat: point.y
+                            };
+                        switch(it.fuel) {
+                            case "Kauko- tai aluelämpö":
+                                building.heatingSystem = "1";
+                                break;
+                            case "Kevyt polttoöljy":
+                                building.heatingSystem = "2";
+                                break;
+                            case "Sähkö":
+                                building.heatingSystem = "3";
+                                break;
+                            default:
+                                building.heatingSystem = "4";
+                                break;
+                        }
+
+                        // 33.5 is the average living space per person in Espoo
+                        building.numberOfInhabitants = Math.round(building.floorArea / 33.5);
+
+                        if (it.solar) {
+                            building.solar = {
+                                ActualArea: parseFloat(it.solar.ActualArea),
+                                AvActKWHm2: parseFloat(it.solar.AvActKWHm2),
+                                RoofArea: parseFloat(it.solar.RoofArea),
+                                RoofAreaAvgIrradiance: parseFloat(it.solar.RoofAreaAvgIrradiance),
+                                RoofGoodArea: parseFloat(it.solar.RoofGoodArea),
+                                RoofGoodAreaAvgIrradiance: parseFloat(it.solar.RoofGoodAreaAvgIrradiance),
+                                RoofRemainingArea: parseFloat(it.solar.RoofRemainingArea),
+                                RoofRemainingAreaAvgIrradiation: parseFloat(it.solar.RoofRemainingAreaAvgIrradiation),
+                                TotalKWh: parseFloat(it.solar.TotalKWh)
                             }
+                        }
+                        building.exteriorPolygon = exteriorPolygon;
 
-                            // 33.5 is the average living space per person in Espoo
-                            building.numberOfInhabitants = Math.round(building.floorArea / 33.5);
+                        return building;
+                });
 
-                            if (it.solar) {
-                                building.solar = {
-                                    ActualArea: parseFloat(it.solar.ActualArea),
-                                    AvActKWHm2: parseFloat(it.solar.AvActKWHm2),
-                                    RoofArea: parseFloat(it.solar.RoofArea),
-                                    RoofAreaAvgIrradiance: parseFloat(it.solar.RoofAreaAvgIrradiance),
-                                    RoofGoodArea: parseFloat(it.solar.RoofGoodArea),
-                                    RoofGoodAreaAvgIrradiance: parseFloat(it.solar.RoofGoodAreaAvgIrradiance),
-                                    RoofRemainingArea: parseFloat(it.solar.RoofRemainingArea),
-                                    RoofRemainingAreaAvgIrradiation: parseFloat(it.solar.RoofRemainingAreaAvgIrradiation),
-                                    TotalKWh: parseFloat(it.solar.TotalKWh)
-                                }
-                            }
-                            building.exteriorPolygon = exteriorPolygon;
-
-                            return building;
+                if (buildings.length === 1) {
+                    buildingSelectionChannel.selectBuilding(buildings[0]);
+                } else if (buildings.length > 1) {
+                    buildingChoiceChannel.setChoices(buildings);
+                } else {
+                    $scope.$apply(function() {
+                        $scope.addressInput = item;
+                        $(".addressInput").focus();
                     });
+                }
+            });
+        };
 
-                    if (buildings.length === 1) {
-                        buildingSelectionChannel.selectBuilding(buildings[0]);
-                    } else if (buildings.length > 1) {
-                        buildingChoiceChannel.setChoices(buildings);
+        $scope.focusedItem = null;
+
+        $scope.hasFocus = function(item) {
+            return $scope.focusedItem === item;
+        };
+        $scope.keyDown = function(event) {
+            var focusIndex = $scope.addressChoices.indexOf($scope.focusedItem),
+                allowBubble = true;
+            switch(event.keyCode) {
+                case 38:
+                    // up
+                    if (focusIndex > 0) {
+                        $scope.focusedItem = $scope.addressChoices[focusIndex - 1];
+                    }
+                    allowBubble = false;
+                    break;
+                case 40:
+                    // down
+                    if (focusIndex < ($scope.addressChoices.length - 1)) {
+                        $scope.focusedItem = $scope.addressChoices[focusIndex + 1];
+                    }
+                    allowBubble = false;
+                    break;
+                case 13:
+                    // enter
+                    if (focusIndex >= 0 && focusIndex < $scope.addressChoices.length) {
+                        $scope.selectItem($scope.addressChoices[focusIndex]);
+                    }
+                    allowBubble = false;
+                    break;
+            }
+            $scope.$digest();
+            return allowBubble;
+        };
+
+        function autocomplete(address) {
+            $.get("/autocomplete", { address: address }).done(function(data) {
+                $scope.$apply(function() {
+                    $scope.addressChoices = JSON.parse(data);
+                    if ($scope.addressChoices.length > 0) {
+                        $scope.focusedItem = $scope.addressChoices[0];
                     }
                 });
+            });
+        }
+
+        $scope.$watch("addressInput", function(newValue) {
+            if (newValue && newValue.length >= 3) {
+                autocomplete(newValue);
+            } else {
+                $scope.addressChoices = [];
+            }
+        })
+        $scope.$watch("address", function(newValue) {
+            if (newValue) {
+
             }
         });
-    }]);
+    }])
+    .directive("addressMatch", [
+        function() {
+            return {
+                restrict: 'A',
+                replace: false,
+                link: function($scope, iElement, attr) {
+                    var html = $scope.$eval(attr.addressMatch).replace(new RegExp("(" + $scope.$eval(attr.input) + ")", "gi"), "<span class='match-text'>$1</span>");
+                    $(iElement).html(html);
+                }
+            }
+        }
+    ])
+    .directive("ngKeydown", [
+        function() {
+            return {
+                restrict: 'A',
+                replace: false,
+                link: function($scope, iElement, attr) {
+                    $(iElement).keydown($scope.$eval(attr.ngKeydown));
+                }
+            }
+        }
+    ]);
